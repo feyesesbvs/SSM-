3c6e60072c447bb500e45ff0947ec13de04d3d54
feyesesbvs <feyesesbvs@163.com>
2017-07-24 16:27:16 +0800

    Update EmployeeService.java

--- src/main/java/com/atguigu/crud/service/EmployeeService.java
+++ src/main/java/com/atguigu/crud/service/EmployeeService.java
@@ -1,8 +1,6 @@
 package com.atguigu.crud.service;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.atguigu.crud.bean.Employee;
